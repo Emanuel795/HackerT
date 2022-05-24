7969156b37562f3788a57e4de95e799296c7d75a
QuantumCode2000 <tancaraninajorgeluis@gmail.com>
2022-05-24 09:56:36 -0400

    refactor code template timetable component

--- src/Routes/TimeTable/TimeTable.jsx
+++ src/Routes/TimeTable/TimeTable.jsx
@@ -1,17 +1,25 @@
 import React, { Fragment } from "react";
-
+import "./TimeTable..styles.css";
 function TimeTable() {
   return (
     <>
-      <div>
-        <span aria-hidden="true">Track 1</span>
-        <span aria-hidden="true">Track 2</span>
-        <span aria-hidden="true">Track 3</span>
-        <span aria-hidden="true">Track 4</span>
+      <div className="container-main">
+        <span className="container-day span2" aria-hidden="true">
+          day 2
+        </span>
+        <span className="container-day span3" aria-hidden="true">
+          day 3
+        </span>
+        <span className="container-day span1" aria-hidden="true">
+          day 1
+        </span>
+        <span className="container-day span4" aria-hidden="true">
+          day 4
+        </span>
 
-        <h2>8:00am</h2>
+        <h2 className="time-ubic time1">8:00am</h2>
 
-        <div>
+        {/* <div>
           <h3>
             <a href="/">Talk Title</a>
           </h3>
@@ -45,39 +53,39 @@ function TimeTable() {
           <span>8:00 - 10:00</span>
           <span>Track: 2</span>
           <span>Presenter</span>
-        </div>
+        </div> */}
 
-        <h2>8:30am</h2>
+        <h2 className="time-ubic time2">8:30am</h2>
 
-        <div>
+        {/* <div>
           <h3>
             <a href="/">Talk Title</a>
           </h3>
           <span>8:30 - 10:00</span>
           <span>Track: 1</span>
           <span>Presenter</span>
-        </div>
+        </div> */}
 
-        <h2>9:00am</h2>
+        <h2 className="time-ubic time3">9:00am</h2>
 
-        <div>
+        {/* <div>
           <h3>
             <a href="/">Talk Title</a>
           </h3>
           <span>9:00 - 10:00</span>
           <span>Track: 1 &amp; 2</span>
           <span>Presenter</span>
-        </div>
+        </div> */}
 
-        <h2>10:00am</h2>
+        <h2 className="time-ubic time4">10:00am</h2>
 
-        <div>
+        {/* <div>
           <h3>Take a break!</h3>
-        </div>
+        </div> */}
 
-        <h2>10:30am</h2>
+        <h2 className="time-ubic time5">10:30am</h2>
 
-        <div>
+        {/* <div>
           <h3>
             <a href="/">Talk Title</a>
           </h3>
@@ -93,20 +101,20 @@ function TimeTable() {
           <span>10:30 - 11:00</span>
           <span>Track: 2 &amp; 3</span>
           <span>Presenter</span>
-        </div>
+        </div> */}
 
-        <div>
+        {/* <div>
           <h3>
             <a href="/">Talk Title</a>
           </h3>
           <span>10:30 - 11:00</span>
           <span>Track: 4</span>
           <span>Presenter</span>
-        </div>
+        </div> */}
 
-        <h2>11:00am</h2>
+        <h2 className="time-ubic time6">11:00am</h2>
 
-        <div>
+        {/* <div>
           <h3>
             <a href="/">Talk Title</a>
           </h3>
@@ -122,7 +130,7 @@ function TimeTable() {
           <span>11:00 - 12:00</span>
           <span>Track: 3</span>
           <span>Presenter</span>
-        </div>
+        </div> */}
       </div>
     </>
   );
